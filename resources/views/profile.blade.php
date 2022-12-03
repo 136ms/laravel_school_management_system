c183ecca073ce315d6f00d61313a7282c6132368
136ms <85730995+136ms@users.noreply.github.com>
2022-12-03 21:10:56 +0100

    [feature] changed profile view

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,62 +1,51 @@
-@section('title'){{$user->fname}} {{$user->lname}}@endsection
+@section('title')
+    {{$user->fname}} {{$user->lname}}
+@endsection
 @extends('layouts.app')
 @section('content')
     <div class="content-header">
-        <div class="container-fluid">
+        <div class="containergit">
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0">{{$user->fname}} {{$user->lname}} profile</h1>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-2">
-                    <div class="card card-primary card-outline">
-                        <div class="card-body box-profile">
-                            <div class="text-center">
-                                <img class="profile-user-img img-fluid img-circle" src="https://assets.infyom.com/logo/blue_logo_150x150.png" alt="User profile picture">
-                            </div>
-                            <h3 class="profile-username text-center">{{$user->fname}} {{$user->lname}}</h3>
-                            <p class="text-muted text-center">4.AI</p>
-                            <a href="/users/{{$user->id}}/edit" class="btn btn-primary btn-block"><b>Edit</b></a>
-                            <a href="#" class="btn btn-primary btn-block"><b>Action button</b></a>
-                            <a href="#" class="btn btn-primary btn-block"><b>Action button</b></a>
-                        </div>
+            <div class="card card-primary card-outline">
+                <div class="card-body box-profile">
+                    <div class="text-center">
+                        <img class="profile-user-img img-fluid img-circle"
+                             src="https://assets.infyom.com/logo/blue_logo_150x150.png"
+                             alt="User profile picture">
                     </div>
-                </div>
-                <div class="col-lg-4">
+                    <h3 class="profile-username text-center">{{$user->fname}} {{$user->lname}}</h3>
+                    <p class="text-muted text-center">4.AI</p>
+                    <a href="/users/{{$user->id}}/edit" class="btn btn-primary btn-block"><b>Edit</b></a>
+                    <a href="#" class="btn btn-primary btn-block"><b>Action button</b></a>
+                    <a href="#" class="btn btn-primary btn-block"><b>Action button</b></a>
+                    <br>
                     <div class="card card-primary">
                         <div class="card-header">
                             <h3 class="card-title">About {{$user->fname}} {{$user->lname}}</h3>
                         </div>
-
                         <div class="card-body">
                             <strong><i class="fas fa-user"></i> First name</strong>
                             <p class="text-muted">{{$user->fname}}</p>
-                            <hr>
                             <strong><i class="fas fa-user"></i> Last name</strong>
                             <p class="text-muted">{{$user->lname}}</p>
-                            <hr>
                             <strong><i class="fas fa-user"></i> Birth date</strong>
                             <p class="text-muted">{{$user->birthdate}}</p>
-                            <hr>
                             <strong><i class="fas fa-user"></i> Address</strong>
                             <p class="text-muted">{{$user->address}}</p>
-                            <hr>
                             <strong><i class="fas fa-user"></i> E-Mail</strong>
                             <p class="text-muted">{{$user->email}}</p>
-                            <hr>
                             <strong><i class="fas fa-user"></i> Gender</strong>
                             <p class="text-muted">{{$user->gender}}</p>
-                            <hr>
                             <strong><i class="fas fa-user"></i> Phone number</strong>
                             <p class="text-muted">{{$user->phonenum}}</p>
-                            <hr>
                             <strong><i class="fas fa-user"></i> Class</strong>
                             <p class="text-muted">todo</p>
-                            <hr>
                             <strong><i class="fas fa-user"></i> Teacher</strong>
                             <p class="text-muted">todo</p>
-                            <hr>
                         </div>
                     </div>
                 </div>
