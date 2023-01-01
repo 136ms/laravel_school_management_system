79e9e88db3d164b926da5e3201fc6a1437b91d67
136ms <85730995+136ms@users.noreply.github.com>
2023-01-01 05:08:52 +0100

    [master] added individual functions with their permissions in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -56,34 +56,36 @@
                                 <h6>Users: {{$userCount}}</h6>
                                 <h6>Subjects: {{$subjectCount}}</h6>
                                 <h6>Groups: {{$groupCount}}</h6>
-
+                            @endcan
+                            @can('users_access')
                                 <a href="{{route('users.index')}}">
                                     <button type="button" class="btn bg-gradient-primary btn-sm">Show users</button>
                                 </a>
+                            @endcan
+                            @can('users_create')
                                 <a href="{{route('users.create')}}">
                                     <button type="button" class="btn bg-gradient-primary btn-sm">Add user</button>
                                 </a>
+                            @endcan
+                            @can('subjects_access')
                                 <a href="{{route('subjects.index')}}">
                                     <button type="button" class="btn bg-gradient-primary btn-sm">Show subjects</button>
                                 </a>
+                            @endcan
+                            @can('subjects_create')
                                 <a href="{{route('subjects.create')}}">
                                     <button type="button" class="btn bg-gradient-primary btn-sm">Add subject</button>
                                 </a>
+                            @endcan
+                            @can('groups_access')
                                 <a href="{{route('groups.index')}}">
                                     <button type="button" class="btn bg-gradient-primary btn-sm">Show groups</button>
                                 </a>
+                            @endcan
+                            @can('groups_create')
                                 <a href="{{route('groups.create')}}">
                                     <button type="button" class="btn bg-gradient-primary btn-sm">Add group</button>
                                 </a>
-                            @endcan
-                            @can('student_access')
-
-                            @endcan
-                            @can('teacher_access')
-
-                            @endcan
-                            @can('parent_access')
-
                             @endcan
                         </div>
                     </div>
