22810ccfd18294400d52a5ec69cfb41f571b59fd
136ms <85730995+136ms@users.noreply.github.com>
2023-01-07 23:54:15 +0100

    [master] added hashing method for password in create() method

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -137,7 +137,7 @@ abstract class BaseRepository
     public function create(array $input): Model
     {
         $model = $this->model->newInstance($input);
-
+        $model['password'] = Hash::make($model['password']);
         $model->save();
 
         return $model;
