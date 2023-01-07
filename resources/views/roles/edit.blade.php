22bfc0bb3594e8c4592fd7e4d4993bfa07c3cad7
136ms <85730995+136ms@users.noreply.github.com>
2023-01-08 00:03:29 +0100

    [master] added view for managing roles

---
+++ resources/views/roles/edit.blade.php
@@ -0,0 +1,45 @@
+@section('title')
+    {{'Edit profile'}}
+@endsection
+@extends('layouts.app')
+
+@section('content')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1>
+                        <i class="fas fa-user"></i>
+                        Manage roles
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="content px-3">
+
+        @include('flash::message')
+        <div class="card">
+            {!! Form::open(['route' => ['users.roles.update',$user->id ], 'method' => 'patch']) !!}
+            <div class="card-body">
+                <div class="row">
+                    @csrf
+                    <input type="hidden" name="user_id" value="{{ $user->id }}">
+                    <div class="form-group">
+                        <h5>{{$user->fullName}}</h5>
+                        {!! Form::label('roles[]', 'Update roles:') !!}
+                        {!! Form::select('roles[]', $roles->pluck('name', 'id'), $user->roles->pluck('id'), ['class' => 'form-control', 'multiple'=> 'multiple']) !!}
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-footer">
+                {!! Form::submit('Update', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('users.edit', $user->id) }}" class="btn btn-default"> Cancel </a>
+            </div>
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
