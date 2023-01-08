c256f7da2eab4532cf9be65be5b783aa97893a01
136ms <85730995+136ms@users.noreply.github.com>
2023-01-08 02:38:24 +0100

    [master] added profile & profiles routes

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,19 @@ Auth::routes();
 
 //Resources
 Route::resource('subjects', App\Http\Controllers\SubjectController::class);
+
 Route::resource('users', App\Http\Controllers\UserController::class);
 Route::get('users/{id}/roles', [App\Http\Controllers\UserController::class, 'roleShow'])->name('users.roles.show');
 Route::patch('users/{id}/roles', [App\Http\Controllers\UserController::class, 'roleUpdate'])->name('users.roles.update');
+
 Route::resource('groups', App\Http\Controllers\GroupController::class);
-Route::resource('profiles', App\Http\Controllers\ProfileController::class);
+
+Route::get('profiles/{id}/edit', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profiles.edit');
+Route::patch('profiles/{id}/update', [App\Http\Controllers\ProfileController::class, 'update'])->name('profiles.update');
+Route::get('profiles/{id}', [App\Http\Controllers\ProfileController::class, 'show'])->name('profiles.show');
+Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile.index');
+Route::get('profile/edit', [App\Http\Controllers\ProfileController::class, 'editUserProfile'])->name('profile.edit');
+Route::patch('profile/update', [App\Http\Controllers\ProfileController::class, 'updateUserProfile'])->name('profile.update');
 
 //Routes
 Route::get('/', function () {
