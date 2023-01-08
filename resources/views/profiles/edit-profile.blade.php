ba695fce8802651e1678e7291bca9d788b329cf9
136ms <85730995+136ms@users.noreply.github.com>
2023-01-08 02:39:21 +0100

    [master] added blade for editing user's profile

---
+++ resources/views/profiles/edit-profile.blade.php
@@ -0,0 +1,42 @@
+@section('title'){{'Edit profile'}}@endsection
+@extends('layouts.app')
+
+@section('content')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1>
+                        <i class="fas fa-user"></i>
+                        Edit Profile
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="content px-3">
+
+        @include('flash::message')
+        @can('roles_update')
+            <a href="{{ route('users.roles.update', $user->id) }}" class="btn btn-primary"> Manage roles </a>
+        @endcan
+        <div class="card">
+            {!! Form::model($user, ['route' => ['profile.update', $user->id], 'method' => 'patch']) !!}
+
+            <div class="card-body">
+                <div class="row">
+                    @include('users.fields')
+                </div>
+            </div>
+
+            <div class="card-footer">
+                {!! Form::submit('Edit', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('profile.index') }}" class="btn btn-default"> Cancel </a>
+            </div>
+
+            {!! Form::close() !!}
+
+        </div>
+    </div>
+@endsection
