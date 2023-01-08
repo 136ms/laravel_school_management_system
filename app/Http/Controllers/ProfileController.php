ddfd4bd49b4a91895759e88ccd1352af47e7f00d
136ms <85730995+136ms@users.noreply.github.com>
2023-01-08 02:33:12 +0100

    [master] added editUserProfile, updateUserProfile methods

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use App\Repositories\UserRepository;
 use Illuminate\Contracts\Foundation\Application;
@@ -11,6 +12,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Hash;
 use Laracasts\Flash\Flash;
 
 class ProfileController extends Controller
@@ -89,6 +91,88 @@ class ProfileController extends Controller
         } else {
             return view('profiles.edit')->with('user', $user);
         }
+    }
+
+    /**
+     * Shows edit User profile view
+     *
+     * @return View|Factory|Redirector|Application|RedirectResponse
+     */
+    public function editUserProfile(): View|Factory|Redirector|Application|RedirectResponse
+    {
+        abort_if(Gate::denies('profile_edit'), 403);
+
+        /** @var User $user */
+        $user = $this->userRepository->find(Auth::id());
+
+        if (!isset($user)) {
+            Flash::error($user->fullName . ' profile was not found.');
+
+            return redirect(route('profiles.index'));
+        } else {
+            return view('profiles.edit-profile')->with('user', $user);
+        }
+    }
+
+    /**
+     * Update User profiles using specified id.
+     *
+     * @param int $id
+     * @param UpdateUserRequest $request
+     * @return Redirector|Application|RedirectResponse
+     */
+    public function update(int $id, UpdateUserRequest $request): Redirector|Application|RedirectResponse
+    {
+        abort_if(Gate::denies('profiles_update'), 403);
+
+        /** @var User $user */
+        $user = $this->userRepository->find($id);
+
+        $input = $request->all();
+
+        $input['password'] = Hash::make($input['password']);
+
+        if (!isset($user)) {
+
+            Flash::error($user->fullName . ' was not found');
+        } else {
+
+            $this->userRepository->update($input, $id);
+
+            Flash::success($user->fullName . ' was updated successfully.');
+        }
+
+        return redirect(route('profile.index'));
+    }
+
+    /**
+     * Updates User profile.
+     *
+     * @param UpdateUserRequest $request
+     * @return Redirector|Application|RedirectResponse
+     */
+    public function updateUserProfile(UpdateUserRequest $request): Redirector|Application|RedirectResponse
+    {
+        abort_if(Gate::denies('profile_update'), 403);
+
+        /** @var User $user */
+        $user = $this->userRepository->find(Auth::id());
+
+        $input = $request->all();
+
+        $input['password'] = Hash::make($input['password']);
+
+        if (!isset($user)) {
+
+            Flash::error($user->fullName . ' was not found');
+        } else {
+
+            $this->userRepository->update($input, Auth::id());
+
+            Flash::success($user->fullName . ' was updated successfully.');
+        }
+
+        return redirect(route('profile.index'));
     }
 }
 
