15517c7a2c53be4e2f181863cbecce0f06dabf1a
136ms <85730995+136ms@users.noreply.github.com>
2023-01-08 02:59:36 +0100

    [master] edited crued, added method descriptions

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateSubjectRequest;
 use App\Http\Requests\UpdateSubjectRequest;
+use App\Models\Subject;
 use App\Repositories\SubjectRepository;
-use App\Repositories\UserRepository;
+use Exception;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Gate;
 use Laracasts\Flash\Flash;
 
@@ -14,127 +20,172 @@ class SubjectController extends AppBaseController
     /** @var SubjectRepository $subjectRepository*/
     private SubjectRepository $subjectRepository;
 
-    /** @var UserRepository $userRepository*/
-    private UserRepository $userRepository;
-
-    public function __construct(SubjectRepository $subjectRepo, UserRepository $userRepository)
+    public function __construct(SubjectRepository $subjectRepo)
     {
         $this->subjectRepository = $subjectRepo;
-        $this->userRepository = $userRepository;
         $this->middleware('auth');
     }
 
+
     /**
-     * Display a listing of the Subject.
+     * Shows index Subjects view
+     *
+     * @return Application|Factory|View
      */
-    public function index()
+    public function index(): View|Factory|Application
     {
         abort_if(Gate::denies('subjects_access'), 403);
+
         $subjects = $this->subjectRepository->paginate(10);
 
-        return view('subjects.index')
-            ->with('subjects', $subjects);
+        if (!isset($subjects)) {
+            Flash::error('Subjects were not found.');
+
+            return view('dashboard');
+        } else {
+
+            return view('subjects.index')
+                ->with('subjects', $subjects);
+        }
     }
 
+
     /**
-     * Show the form for creating a new Subject.
+     * Shows create Subject view
+     *
+     * @return Factory|View|Application
      */
-    public function create()
+    public function create(): Factory|View|Application
     {
         abort_if(Gate::denies('subjects_create'), 403);
+
         return view('subjects.create');
     }
 
+
     /**
-     * Store a newly created Subject in storage.
+     * Creates new Subject
+     *
+     * @param CreateSubjectRequest $request
+     * @return Redirector|Application|RedirectResponse
      */
-    public function store(CreateSubjectRequest $request)
+    public function store(CreateSubjectRequest $request): Redirector|Application|RedirectResponse
     {
         abort_if(Gate::denies('subjects_store'), 403);
+
         $input = $request->all();
 
-        $subject = $this->subjectRepository->create($input);
+        if (!isset($input)) {
+            Flash::error('Subject was not created successfully.');
+        } else {
+            $subject = $this->subjectRepository->create($input);
 
-        Flash::success('Subject saved successfully.');
+            Flash::success($subject->name . ' was created successfully.');
+        }
 
-        return redirect(route('subjects.index'));
+        return redirect(route('groups.index'));
     }
 
+
     /**
-     * Display the specified Subject.
+     * Shows show Subject view using specified id
+     *
+     * @param int $id
+     * @return View|Factory|Redirector|Application|RedirectResponse
      */
-    public function show($id)
+    public function show(int $id): View|Factory|Redirector|Application|RedirectResponse
     {
         abort_if(Gate::denies('subjects_show'), 403);
+
+        /** @var Subject $subject */
         $subject = $this->subjectRepository->find($id);
 
-        if (empty($subject)) {
-            Flash::error('Subject not found');
+        if (!isset($subject)) {
+            Flash::error($subject->name . ' was not found');
 
             return redirect(route('subjects.index'));
+        } else {
+            return view('subjects.show')->with('subject', $subject);
         }
-
-        return view('subjects.show')->with('subject', $subject);
     }
 
+
     /**
-     * Show the form for editing the specified Subject.
+     * Shows edit Subject view using specified id
+     *
+     * @param int $id
+     * @return View|Factory|Redirector|Application|RedirectResponse
      */
-    public function edit($id)
+    public function edit(int $id): View|Factory|Redirector|Application|RedirectResponse
     {
         abort_if(Gate::denies('subjects_edit'), 403);
+
+        /** @var Subject $subject */
         $subject = $this->subjectRepository->find($id);
 
-        if (empty($subject)) {
-            Flash::error('Subject not found');
+        if (!isset($subject)) {
+            Flash::error($subject->name . ' was not found');
 
             return redirect(route('subjects.index'));
+        } else {
+            return view('subjects.edit')->with('subject', $subject);
         }
-
-        return view('subjects.edit')->with('subject', $subject);
     }
 
+
     /**
-     * Update the specified Subject in storage.
+     * Updates User Subject using specified id
+     *
+     * @param int $id
+     * @param UpdateSubjectRequest $request
+     * @return Redirector|Application|RedirectResponse
      */
-    public function update($id, UpdateSubjectRequest $request)
+    public function update(int $id, UpdateSubjectRequest $request): Redirector|Application|RedirectResponse
     {
         abort_if(Gate::denies('subjects_update'), 403);
+
+        /** @var Subject $subject */
         $subject = $this->subjectRepository->find($id);
 
-        if (empty($subject)) {
-            Flash::error('Subject not found');
+        $input = $request->all();
 
-            return redirect(route('subjects.index'));
+        if (!isset($subject)) {
+            Flash::error($subject->name . ' was not found');
+        } else {
+            $this->subjectRepository->update($input, $id);
+
+            Flash::success($subject->name . ' was updated successfully.');
         }
-
-        $subject = $this->subjectRepository->update($request->all(), $id);
-
-        Flash::success('Subject updated successfully.');
 
         return redirect(route('subjects.index'));
     }
 
+
     /**
-     * Remove the specified Subject from storage.
+     * Removes Subject using specified id.
      *
-     * @throws \Exception
+     * @param $id
+     * @return Redirector|Application|RedirectResponse
+     * @throws Exception
      */
-    public function destroy($id)
+    public function destroy($id): Redirector|Application|RedirectResponse
     {
         abort_if(Gate::denies('subjects_destroy'), 403);
+
+        /** @var Subject $subject */
         $subject = $this->subjectRepository->find($id);
 
-        if (empty($subject)) {
-            Flash::error('Subject not found');
+        if (!isset($subject)) {
 
-            return redirect(route('subjects.index'));
+            Flash::error($subject->name . ' was not found');
+
+        } else {
+
+            $this->subjectRepository->delete($id);
+
+            Flash::success($subject->name . ' was deleted successfully.');
+
         }
-
-        $this->subjectRepository->delete($id);
-
-        Flash::success('Subject deleted successfully.');
-
         return redirect(route('subjects.index'));
     }
 }
