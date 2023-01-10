ef8e1f1a0f8f66035927dba6cec3a0f3c6538e62
136ms <85730995+136ms@users.noreply.github.com>
2023-01-10 21:30:47 +0100

    [master] Removed unnecessary div

--- resources/views/groups/fields.blade.php
+++ resources/views/groups/fields.blade.php
@@ -3,9 +3,3 @@
     {!! Form::label('name', 'Name:') !!}
     {!! Form::text('name', null, ['class' => 'form-control', 'required', 'maxlength' => 255]) !!}
 </div>
-
-<!-- Users Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('users', 'Group users:') !!}
-    {!! Form::text('users', null, ['class' => 'form-control']) !!}
-</div>
