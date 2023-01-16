9960d13cc824a44ec120214c84457e3ea5b2a59b
136ms <85730995+136ms@users.noreply.github.com>
2023-01-16 19:04:34 +0100

    [master] edited view for profile picture upload

--- resources/views/users/manage-user-picture.blade.php
+++ resources/views/users/manage-user-picture.blade.php
@@ -10,22 +10,37 @@
                 <div class="col-sm-12">
                     <h1>
                         <i class="fas fa-user"></i>
-                        Manage user teachers
+                        Update profile picture
                     </h1>
                 </div>
             </div>
         </div>
     </section>
-
-    <div class="content px-3">
-
+    <div class="col-sm-12">
         @include('flash::message')
-        <div class="card">
-            <form action="{{route('users.pictures.update')}}" method="POST" enctype="multipart/form-data">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Upload profile picture</h3>
+            </div>
+            <form action="{{route('user.avatar.update')}}" method="POST" enctype="multipart/form-data">
                 @csrf
-                <input type="file" name="avatar">
-                <input type="submit" value="Upload">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="exampleInputFile">Picture upload</label>
+                        <div class="input-group">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" name="avatar">
+                                <label class="custom-file-label" for="avatar">Choose picture</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
             </form>
         </div>
     </div>
+
 @endsection
