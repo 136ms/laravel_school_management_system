c34ffffb22524483390588cedec1bc3bc65368f2
136ms <85730995+136ms@users.noreply.github.com>
2023-01-20 20:39:10 +0100

    [master] added edit and show blade for Grades

---
+++ resources/views/grades/edit.blade.php
@@ -0,0 +1,41 @@
+@section('title')
+    {{'Edit grade'}}
+@endsection
+@extends('layouts.app')
+
+@section('content')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1>
+                        <i class="fas fa-user"></i>
+                        Edit Grade
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="content px-3">
+
+        @include('flash::message')
+        <div class="card">
+            {!! Form::model($grade, ['route' => ['grades.update', $grade->id], 'method' => 'patch']) !!}
+
+            <div class="card-body">
+                <div class="row">
+                    @include('grades.fields')
+                </div>
+            </div>
+
+            <div class="card-footer">
+                {!! Form::submit('Edit', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('grades.index') }}" class="btn btn-default"> Cancel </a>
+            </div>
+
+            {!! Form::close() !!}
+
+        </div>
+    </div>
+@endsection
---
+++ resources/views/grades/show.blade.php
@@ -0,0 +1,64 @@
+@section('title')
+    {{$grade->name}}
+@endsection
+@extends('layouts.app')
+
+@section('content')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>
+                        <i class="fas fa-user"></i>
+                        {{$grade->name}}
+                    </h1>
+                </div>
+                <div class="col-sm-6">
+                    <a class="btn btn-default float-right"
+                       href="{{ route('grades.index') }}">
+                        Back
+                    </a>
+                    <a class="btn btn-primary float-right mr-1" id="print-button">
+                        Print
+                    </a>
+                </div>
+            </div>
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">About {{$grade->name}}</h3>
+                </div>
+                <div class="card-body" id="print-data">
+                    @include('flash::message')
+
+                    <strong>ID</strong>
+                    <p class="text-muted">{{$grade->id}}</p>
+
+                    <strong>Name</strong>
+                    <p class="text-muted">{{$grade->name}}</p>
+
+                    <strong>Weight</strong>
+                    <p class="text-muted">{{$grade->weight}}</p>
+
+                    <strong>Grade</strong>
+                    <p class="text-muted">{{$grade->grade}}</p>
+
+                    <strong>Created at</strong>
+                    <p class="text-muted">{{$grade->created_at}}</p>
+
+                    <strong>Updated at</strong>
+                    <p class="text-muted">{{$grade->updated_at}}</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script>
+        document.getElementById("print-button").addEventListener("click", function(){
+            var dataToPrint = document.getElementById("print-data");
+            window.document.write(dataToPrint.innerHTML);
+            window.print();
+            setTimeout(function(){
+                window.history.back();
+            });
+        });
+    </script>
+@endsection
