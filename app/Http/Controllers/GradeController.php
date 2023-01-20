c65edff9b6d276545ae8187a1d1a06d80a12b045
136ms <85730995+136ms@users.noreply.github.com>
2023-01-20 20:38:52 +0100

    [master] added show, edit, update, destroy methods to GradeController

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -7,6 +7,7 @@ use App\Repositories\GradeRepository;
 use App\Repositories\GroupRepository;
 use App\Repositories\SubjectRepository;
 use App\Repositories\UserRepository;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -101,6 +102,112 @@ class GradeController extends Controller
             Flash::success('Grade was created successfully.');
             return redirect(route('grades.index'));
         }
+    }
+
+    /**
+     * Shows show Grade view using specified id
+     *
+     * @param int $id
+     * @return View|Factory|Redirector|Application|RedirectResponse
+     */
+    public function show(int $id): View|Factory|Redirector|Application|RedirectResponse
+    {
+        abort_if(Gate::denies('grades_show'), 403);
+
+        /** @var Grade $grade */
+        $grade = $this->gradeRepository->find($id);
+
+        if (!isset($grade)) {
+
+            Flash::error($grade->name . ' was not found');
+
+            return redirect(route('grades.index'));
+        } else {
+            return view('grades.show')->with(['grade' => $grade]);
+        }
+    }
+
+    /**
+     * Shows edit Grade view using specified id
+     *
+     * @param int $id
+     * @return View|Factory|Redirector|Application|RedirectResponse
+     */
+    public function edit(int $id): View|Factory|Redirector|Application|RedirectResponse
+    {
+        abort_if(Gate::denies('grades_edit'), 403);
+
+        /** @var Grade $grade */
+        $grade = $this->gradeRepository->find($id);
+        $users = $this->userRepository->all();
+
+        if (!isset($grade)) {
+            Flash::error($grade->name . ' was not found');
+
+            return redirect(route('grades.index'));
+        } else {
+            return view('grades.edit')->with([
+                'grade' => $grade,
+                'users' => $users
+            ]);
+        }
+    }
+
+    /**
+     * Updates Grade using specified id.
+     *
+     * @param int $id
+     * @param Request $request
+     * @return Redirector|Application|RedirectResponse
+     */
+    public function update(int $id, Request $request): Redirector|Application|RedirectResponse
+    {
+        abort_if(Gate::denies('grades_update'), 403);
+
+        /** @var Grade $grade */
+        $grade = $this->gradeRepository->find($id);
+
+        $input = $request->all();
+
+        if (!isset($grade)) {
+
+            Flash::error($grade->name . ' was not found');
+        } else {
+
+            $this->gradeRepository->update($input, $id);
+
+            Flash::success($grade->name . ' was updated successfully.');
+        }
+
+        return redirect(route('grades.index'));
+    }
+
+    /**
+     * Removes Grade using specified id.
+     *
+     * @param int $id
+     * @return Redirector|Application|RedirectResponse
+     * @throws Exception
+     */
+    public function destroy(int $id): Redirector|Application|RedirectResponse
+    {
+        abort_if(Gate::denies('grades_destroy'), 403);
+
+        /** @var Grade $grade */
+        $grade = $this->gradeRepository->find($id);
+
+        if (!isset($grade)) {
+
+            Flash::error($grade->name . ' was not found');
+
+        } else {
+
+            $this->gradeRepository->delete($id);
+
+            Flash::success($grade->name . ' was deleted successfully.');
+
+        }
+        return redirect(route('grades.index'));
     }
 }
 
