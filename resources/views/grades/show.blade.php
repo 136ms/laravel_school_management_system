f773818674f8a55eceb95a7a5d0aaeb3fcb5a3b4
136ms <85730995+136ms@users.noreply.github.com>
2023-01-25 00:37:00 +0100

    [master] added print grades @can

--- resources/views/grades/show.blade.php
+++ resources/views/grades/show.blade.php
@@ -19,9 +19,11 @@
                            href="{{ route('grades.index') }}">
                             Back
                         </a>
-                        <a class="btn btn-primary float-right mr-1" id="print-button">
-                            Print
-                        </a>
+                        @can('print_grades')
+                            <a class="btn btn-primary float-right mr-1" id="print-button">
+                                Print
+                            </a>
+                        @endcan
                     </div>
                 </div>
                 <div class="card card-primary">
