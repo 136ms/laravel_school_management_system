015d71c4148de22abbde1ef3a18a736305ae841e
136ms <85730995+136ms@users.noreply.github.com>
2023-01-24 15:55:29 +0100

    [master] added users_create permission to users create blade

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,46 +4,48 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-12">
-                    <h1>
-                        <i class="fas fa-user"></i>
-                        Add User
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <div class="content px-3">
-
-        @include('flash::message')
-
-        <div class="card">
-
-            {!! Form::open(['route' => 'users.store']) !!}
-
-            <div class="card-body">
-
-                <div class="row">
-                    @include('users.fields')
-                    <div class="form-group col-sm-6">
-                        {!! Form::label('roles[]', 'Roles:') !!}
-                        {!! Form::select('roles[]', $roles->pluck('name', 'id'), null, ['class' => 'form-control', 'multiple'=> 'multiple']) !!}
+    @can('users_create')
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-12">
+                        <h1>
+                            <i class="fas fa-user"></i>
+                            Add User
+                        </h1>
                     </div>
                 </div>
+            </div>
+        </section>
+
+        <div class="content px-3">
+
+            @include('flash::message')
+
+            <div class="card">
+
+                {!! Form::open(['route' => 'users.store']) !!}
+
+                <div class="card-body">
+
+                    <div class="row">
+                        @include('users.fields')
+                        <div class="form-group col-sm-6">
+                            {!! Form::label('roles[]', 'Roles:') !!}
+                            {!! Form::select('roles[]', $roles->pluck('name', 'id'), null, ['class' => 'form-control', 'multiple'=> 'multiple']) !!}
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="card-footer">
+                    {!! Form::submit('Add', ['class' => 'btn btn-primary']) !!}
+                    <a href="{{ route('users.index') }}" class="btn btn-default"> Cancel </a>
+                </div>
+
+                {!! Form::close() !!}
 
             </div>
-
-            <div class="card-footer">
-                {!! Form::submit('Add', ['class' => 'btn btn-primary']) !!}
-                <a href="{{ route('users.index') }}" class="btn btn-default"> Cancel </a>
-            </div>
-
-            {!! Form::close() !!}
-
         </div>
-    </div>
+    @endcan
 @endsection
