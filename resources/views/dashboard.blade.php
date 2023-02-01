a62afc7f309e39642b117557018b835d485d7577
136ms <85730995+136ms@users.noreply.github.com>
2023-02-01 20:13:15 +0100

    [master] modified dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,15 +51,94 @@
                             </div>
                         </div>
                     </div>
-
-                    <div class="col-sm-8">
+                    @can('student_widget')
+                        <div class="col-sm-4">
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h3 class="card-title">Grades</h3>
+                                </div>
+                                @include('grades.table')
+                            </div>
+                        </div>
+                    @endcan
+                    <div class="col-sm-4">
                         <div class="card card-primary">
                             <div class="card-header">
-                                <h3 class="card-title">Dashboard</h3>
+                                <h3 class="card-title">Functions</h3>
                             </div>
                             <div class="card-body">
-                                @can('admin_access')
-
+                                <h5>Lists</h5>
+                                @can('subjects_access')
+                                    <a href="{{ route('subjects.index', [$subjects]) }}"
+                                       class='btn btn-outline-primary'><i class="fas fa-book text-purple"></i>
+                                        Subjects
+                                    </a>
+                                @endcan
+                                @can('groups_access')
+                                    <a href="{{ route('groups.index', [$groups]) }}"
+                                       class='btn btn-outline-primary'><i class="fa fa-users text-info"></i>
+                                        Groups
+                                    </a>
+                                @endcan
+                                @can('grades_access')
+                                    <a href="{{ route('grades.index', [$grades]) }}"
+                                       class='btn btn-outline-primary'><i class="fas fa-graduation-cap text-yellow"></i>
+                                        Grades
+                                    </a>
+                                @endcan
+                                @can('children_access')
+                                    <a href="{{ route('children.index', [$children]) }}"
+                                       class='btn btn-outline-primary'><i class="fas fa-child text-pink"></i>
+                                        Children
+                                    </a>
+                                @endcan
+                            </div>
+                            <div class="card-body">
+                                <h5>Create</h5>
+                                @can('subjects_create')
+                                    <a href="{{ route('subjects.create', [$subjects]) }}"
+                                       class='btn btn-outline-primary'><i class="fa fa-book text-purple"></i>
+                                        Subject
+                                    </a>
+                                @endcan
+                                @can('groups_create')
+                                    <a href="{{ route('groups.create', [$groups]) }}"
+                                       class='btn btn-outline-primary'><i class="fa fa-users text-info"></i>
+                                        Group
+                                    </a>
+                                @endcan
+                                @can('grades_create')
+                                    <a href="{{ route('grades.create', [$grades]) }}"
+                                       class='btn btn-outline-primary'><i class="fas fa-graduation-cap text-yellow"></i>
+                                        Grade
+                                    </a>
+                                @endcan
+                                @can('users_create')
+                                    <a href="{{ route('users.create', [$user]) }}"
+                                       class='btn btn-outline-primary'><i class="fas fa-users text-lime"></i>
+                                        User
+                                    </a>
+                                @endcan
+                            </div>
+                            <div class="card-body">
+                                <h5>User</h5>
+                                @can('profiles_access')
+                                    <a href="{{ route('profile.index') }}"
+                                       class='btn btn-outline-primary'><i class="fa fa-user text-purple"></i>
+                                        Profile
+                                    </a>
+                                @endcan
+                                @can('profile_edit')
+                                    <a href="{{ route('profile.edit') }}"
+                                       class='btn btn-outline-primary'><i class="fa fa-edit text-purple"></i>
+                                        Edit profile
+                                    </a>
+                                @endcan
+                                @can('user_picture_edit')
+                                    <a href="{{ route('user.avatar') }}"
+                                       class='btn btn-outline-primary'><i class="fa fa-edit text-purple"></i>
+                                        Profile avatar
+                                    </a>
                                 @endcan
                             </div>
                         </div>
