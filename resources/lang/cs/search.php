44d27e4ea0c976eed94e6c0e0df652894a8f86af
136ms <85730995+136ms@users.noreply.github.com>
2023-04-12 22:28:30 +0200

    [master] added searchbar translations

---
+++ resources/lang/cs/search.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'searchUsers' => "Vyhledávejte pomocí křestního jména, příjmení, adresy, e-mailu nebo telefonního čísla:",
+    'searchSubjects' => "Vyhledávejte pomocí názvu předmětu:",
+    'searchGroups' => "Vyhledávejte pomocí názvu třídy:",
+    'searchGrades' => "Vyhledávejte pomocí názvu známky, známky, váhy:",
+    'searchChildren' => "Vyhledávejte pomocí křestního jména, příjmení:",
+];
---
+++ resources/lang/en/search.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'searchUsers' => "Search using first name, last name, address, e-mail or phone number:",
+    'searchSubjects' => "Search using subject name:",
+    'searchGroups' => "Search using group name:",
+    'searchGrades' => "Search using grade name:",
+    'searchChildren' => "Search using first name, last name:",
+];
